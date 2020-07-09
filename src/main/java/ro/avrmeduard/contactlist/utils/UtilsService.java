01cb3eccb4ed27b3718f885f50519a2ff31547e0
avrmeduard <avrmeduard@gmail.com>
2020-07-09 20:18:53 +0300

    method to create a list of contact strings from a list of users

--- src/main/java/ro/avrmeduard/contactlist/utils/UtilsService.java
+++ src/main/java/ro/avrmeduard/contactlist/utils/UtilsService.java
@@ -9,4 +9,32 @@ import java.util.Map;
 
 public class UtilsService {
 
+    public static List <String> contactToString(List<? extends User> objects) {
+
+        List<String> userInLine = new ArrayList <>();
+        StringBuilder line = new StringBuilder();
+
+        for (User u : objects) {
+
+            line.append(u.getUserID() + "|" + u.getFirstName() + "|" + u.getLastName() + "|" + u.getEmail() + "|" + u.getAge() + "|");
+            int count = 1;
+
+            for (Map.Entry<String, PhoneNumber> entry : u.getPhoneNumbers().entrySet()) {
+                line.append(entry.getKey() + "_" + entry.getValue().toString());
+
+                if (count < u.getPhoneNumbers().entrySet().size()) {
+                    line.append(";");
+                }
+                count++;
+            }
+
+            line.append("|" + u.getAddress().toString());
+            line.append("|" + u.getCompany().getName() + "_" + u.getCompany().getAddress().toString());
+            line.append("|" + u.getIsFavorite());
+            userInLine.add(line.toString());
+            line = new StringBuilder();
+        }
+        return userInLine;
+    }
+
 }
