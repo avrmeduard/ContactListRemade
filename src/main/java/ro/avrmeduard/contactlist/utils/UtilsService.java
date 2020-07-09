90be82d1af1a42bd2fd189b9b2885c5da3467b39
avrmeduard <avrmeduard@gmail.com>
2020-07-09 20:16:39 +0300

    Utils Service Classes created

---
+++ src/main/java/ro/avrmeduard/contactlist/utils/UtilsService.java
@@ -0,0 +1,12 @@
+package ro.avrmeduard.contactlist.utils;
+
+import ro.avrmeduard.contactlist.model.PhoneNumber;
+import ro.avrmeduard.contactlist.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class UtilsService {
+
+}
