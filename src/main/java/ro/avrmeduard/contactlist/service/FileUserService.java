404057c513a049c9eeb0add9fe71b5b661e7b49f
avrmeduard <avrmeduard@gmail.com>
2020-07-10 20:32:34 +0300

    File user service class implements interface UserService

--- src/main/java/ro/avrmeduard/contactlist/service/FileUserService.java
+++ src/main/java/ro/avrmeduard/contactlist/service/FileUserService.java
@@ -1,4 +1,41 @@
 package ro.avrmeduard.contactlist.service;
 
-public class FileUserService {
+import ro.avrmeduard.contactlist.model.Address;
+import ro.avrmeduard.contactlist.model.Company;
+import ro.avrmeduard.contactlist.model.PhoneNumber;
+import ro.avrmeduard.contactlist.model.User;
+
+import java.util.List;
+import java.util.Map;
+
+public class FileUserService implements UserService {
+    @Override
+    public List <User> getContacts() {
+        return null;
+    }
+
+    @Override
+    public User getContactById(int userId) {
+        return null;
+    }
+
+    @Override
+    public void addContact(User contact) {
+
+    }
+
+    @Override
+    public void editContact(int userId , String firstName , String lastName , String email , Integer age , Map <String, PhoneNumber> phoneNumber , Address address , String jobTitle , Company company , boolean isFavorite) {
+
+    }
+
+    @Override
+    public void removeContact(int userId) {
+
+    }
+
+    @Override
+    public List <User> search(String query) {
+        return null;
+    }
 }
