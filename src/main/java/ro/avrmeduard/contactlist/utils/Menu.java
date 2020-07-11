31a24add237db59eb2fbe3997960e4f034c3b2c2
avrmeduard <avrmeduard@gmail.com>
2020-07-11 17:54:46 +0300

    Menu class added for printing menu

--- src/main/java/ro/avrmeduard/contactlist/utils/Menu.java
+++ src/main/java/ro/avrmeduard/contactlist/utils/Menu.java
@@ -11,5 +11,18 @@ public class Menu {
     private static Scanner scanner = new Scanner(System.in);
     private static List <User> users = new ArrayList <>();
 
+    public void startPhone() {
+        System.out.println("Starting phone ...");
+    }
+
+    public void printMenu() {
+        System.out.println("\nAvailable actions:\npress");
+        System.out.println("0 - to shutdown\n" +
+                "1 - to print contacts\n" +
+                "2 - to print contacts details\n" +
+                "3 - to print favorite list\n" +
+                "4 - to print a list of available actions.");
+        System.out.println("Choose your actions: ");
+    }
 
 }
