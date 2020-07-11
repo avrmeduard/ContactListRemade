80947b54ea0fd952b53e1eb79ff8aed2dc86f4e9
avrmeduard <avrmeduard@gmail.com>
2020-07-11 17:50:21 +0300

    Menu class added for printing menu

---
+++ src/main/java/ro/avrmeduard/contactlist/utils/Menu.java
@@ -0,0 +1,15 @@
+package ro.avrmeduard.contactlist.utils;
+
+import ro.avrmeduard.contactlist.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Menu {
+
+    private static Scanner scanner = new Scanner(System.in);
+    private static List <User> users = new ArrayList <>();
+
+
+}
