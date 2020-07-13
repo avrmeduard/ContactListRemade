08710d4e8c123d5873f87effc074cbafc56f057b
avrmeduard <avrmeduard@gmail.com>
2020-07-13 21:05:40 +0300

    print menu

--- src/main/java/ro/avrmeduard/contactlist/Main.java
+++ src/main/java/ro/avrmeduard/contactlist/Main.java
@@ -6,15 +6,16 @@ import ro.avrmeduard.contactlist.service.UserService;
 public class Main {
 
     public static void main(String[] args) {
-
+        // initialize db
         UserService service = new DBUserService();
+
+        Menu menu = new Menu();
+        // print menu
+        menu.printActionMenu();
+
 
         //print db contacts
         service.getContacts()/*.forEach(System.out::println)*/;
-
-        System.out.println("\n\n\n\n\n\n\n\n");
-
-
         //get db contact by id
         System.out.println(service.getContactById(4).toString());
 
