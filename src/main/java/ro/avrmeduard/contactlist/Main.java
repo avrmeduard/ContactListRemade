8ec10453497a73f9ed737d307b731570ed3ddbd9
avrm <avrmeduard@gmail.com>
2020-07-18 04:47:19 +0300

    print menu

--- src/main/java/ro/avrmeduard/contactlist/Main.java
+++ src/main/java/ro/avrmeduard/contactlist/Main.java
@@ -1,23 +1,27 @@
 package ro.avrmeduard.contactlist;
 
-import ro.avrmeduard.contactlist.service.DBUserService;
-import ro.avrmeduard.contactlist.service.UserService;
-
 public class Main {
 
     public static void main(String[] args) {
         // initialize db
-        UserService service = new DBUserService();
 
-        Menu menu = new Menu();
-        // print menu
-        menu.printActionMenu();
+        Main main = new Main();
+
+
+//        UserService service = new DBUserService();
+
+//        FileUserService fileUserService = new FileUserService("Contacts.txt");
+
+//        Menu menu = new Menu();
+//        // print menu
+//        menu.printActionMenu();
+
 
 
         //print db contacts
-        service.getContacts()/*.forEach(System.out::println)*/;
+        //service.getContacts()/*.forEach(System.out::println)*/;
         //get db contact by id
-        System.out.println(service.getContactById(4).toString());
+        //System.out.println(service.getContactById(4).toString());
 
 
     }
